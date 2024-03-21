f61fbe3a7069f5d63d3aaaf136739750c7937162
jsmeng324 <mengjs324@gmail.com>
2024-03-21 13:20:45 -0400

    add canvas drawing on top of image

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
 };
 
 export default nextConfig;
