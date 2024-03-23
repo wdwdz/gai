3d2bce341776a6973baf8516a18a718a1b183569
jsmeng324 <mengjs324@gmail.com>
2024-03-22 21:43:43 -0400

    exclude canvas lib from build

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
-// For building node-canvas on vercel: https://github.com/Automattic/node-canvas/issues/1779
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
+  },
+  experimental: {
+    legacyBrowsers: false,
+    outputFileTracingExcludes: ['**canvas**']
   },
   webpack: (config) => {
     config.externals.push({
