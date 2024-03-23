0c91b556250849077953cd9bf100a205cb4fb23c
jsmeng324 <mengjs324@gmail.com>
2024-03-22 21:18:39 -0400

    move the node-canvas build snippet up

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-
 // For building node-canvas on vercel: https://github.com/Automattic/node-canvas/issues/1779
 if (
   process.env.LD_LIBRARY_PATH == null ||
@@ -12,6 +10,7 @@ if (
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
